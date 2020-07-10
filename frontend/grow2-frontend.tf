7607c515179c225d1370eada9f89b10617498d27
Geoff Nelson <geoffreysnelson@gmail.com>
2020-07-10 12:44:40 -0700

    fixed FE to do initial stanup and deploy.  Added new keys.

---
+++ frontend/grow2-frontend.tf
@@ -0,0 +1,42 @@
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_key_pair" "FETerraform" {
+  key_name   = "FETerraform"
+  public_key = file("../FETerraform.pub")
+}
+
+resource "aws_instance" "frontend" {
+  key_name               = aws_key_pair.FETerraform.key_name
+  ami                    = "ami-002a48030440e00da"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["sg-05ea20d5ee2e9549e"]
+
+  tags = {
+    Name = "FEGrowV1Terraform"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "alpine"
+    private_key = file("../FETerraform")
+    host        = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apk add --update npm",
+      "wget https://github.com/geoffn/grow-public/archive/master.zip",
+      "unzip master.zip",
+      "cd grow-public-master",
+      "npm install",
+      "sudo npm install forever -g",
+      "forever start ./src/app.js"
+
+
+    ]
+  }
+}
+
